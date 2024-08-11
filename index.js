9726bcd36a92ee24ca84e125dd25bb5a70aff200
Nick Karamanian <nick.karamanian@gmail.com>
2024-08-11 16:30:29 -0700

    server added

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const axios = require('axios')
+require("dotenv").config()
+
+PORT = process.env.PORT || 5000
+RECIPE_API = `${process.env.RECIPE_API}`
+
+const { rateLimit } = require('express-rate-limit')
+const limiter = rateLimit({
+    windowMs: 2000,
+    limit: 1,
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+    message: 'Requesting too quick, please wait.'
+})
+
+app.use(cors())
+app.use(limiter)
+
+app.get('/recipes/:query', async (req,res) => {
+    try{
+        const response = await axios.get(
+            `${RECIPE_API}?type=public&q=${req.params.query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`);
+        
+        res.status(200).json(response.data.hits);
+    }
+    catch(error)    {
+        res.status(400).json({message:"Error Fetching Data"})
+    }
+})
+
+app.all("*",(req,res) => {
+    res.status(404).json({message: "URL Not Found."})
+})
+
+app.listen(PORT, (err) => {
+    if (err)    {
+        console.log("Server Connection Error"); 
+    }
+    console.log(`Listening on port ${PORT}`);
+})
